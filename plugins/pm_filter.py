5af9f9624e4a095ebc3d5012b9d7062b36ed8905
omgoog <89709117+omgoog@users.noreply.github.com>
2021-12-13 03:15:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -749,8 +749,8 @@ async def advantage_spell_chok(msg):
                     text=movie.strip(),
                     callback_data=f"spolling#{user}#{k}",
                 )
-            ] for k, movie in enumerate(movielist)
-    btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')]
+            ] for k, movie in enumerate(movielist)]
+    btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
     await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?", reply_markup=InlineKeyboardMarkup(btn))
     
 
